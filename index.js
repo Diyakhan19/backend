3a72852945825843c688608be8815a76c2d17c26
Fayiz Ahmad <fayizkhan9@gmail.com>
2024-07-16 00:04:35 +0500

    File upload done

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 
 app.use(express.json());
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const staticFolderPath = path.join(__dirname, "uploads");
+app.use("/uploads", express.static(staticFolderPath));
 
 import authRouter from "./src/api/auth/auth.router.js";
 import userRouter from "./src/api/user/user.router.js";
 
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
+
+app.get("/api", (req, res) => {
+  return res.send("Server is running...!");
+});
 
 // Error handler
 app.use((err, req, res, next) => {
--- src/api/auth/auth.router.js
+++ src/api/auth/auth.router.js
@@ -3,9 +3,15 @@ import { login, signup } from "./auth.controller.js";
 const router = express.Router();
 import { validate } from "../../utils/shema.validation.js";
 import { sigupSchema, loginSchema } from "./auth.schema.js";
+import upload from "../../middleware/upload.middleware.js";
 
 // Signup route
-router.post("/signup", validate(sigupSchema), signup);
+router.post(
+  "/signup",
+  upload.profileImg.single("image"),
+  validate(sigupSchema),
+  signup
+);
 
 // Login route
 router.post("/login", validate(loginSchema), login);
